42427085972a3657c4360c92347787786dd2fc27
alec-host <irualec@yahoo.com>
2024-02-14 15:55:33 +0300

    3rd commit - added routing script - /src/index.js

--- src/index.js
+++ src/index.js
@@ -4,10 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+	<BrowserRouter>
+		<Routes>
+			<Route path='/' element={<Home/>} />
+			<Route path='/login' element={<Login/>} />
+		</Routes>
+	</BrowserRouter>
   </React.StrictMode>
 );
 
