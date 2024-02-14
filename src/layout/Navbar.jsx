6461e10da4e9ca5be812a948b62888c466be8659
alec-host <irualec@yahoo.com>
2024-02-14 21:56:55 +0300

    7th commit: made update to public/index.html public/manifest.json, modified scripts src/layout/Footer.jsx, src/layout Navbar.jsx, src/pages/Home.jsx, src/pages/Login.jsx

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -2,7 +2,21 @@ import React from "react";
 
 const Navbar = () => {
     return(
-        <><h1>NAVBAR</h1></>
+        <>
+            <div className="lg:flex lg:items-center lg:justify-between bg-[#F8F4EE]">
+                <div className="min-w-0 flex-1">
+                    <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
+                        <img className="h-20 w-40 mx-5" src="https://assets-global.website-files.com/6304f37ec5e0ff0b9beb4b6b/6304f3f51e1ba82f2a01eee6_Asset%201.svg" alt="logo"/>
+                    </h2>
+                    <div className="mt-1 flex flex-col sm:mt-0 sm:flex-row sm:flex-wrap sm:space-x-6">
+                        <div className="mt-2 flex items-center text-sm text-gray-500">
+                        </div>
+                    </div>
+                </div>
+                <div className="mt-5 flex lg:ml-4 lg:mt-0">
+                </div>
+            </div>
+        </>
     );
 };
 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -29,7 +29,6 @@ const LoginTemplate = () => {
             formData.email = inputEmail?.current.value;
             formData.password = inputPassword?.current.value;
 
-            
             console.log(formData);
 
         }else{
@@ -41,8 +40,8 @@ const LoginTemplate = () => {
 		<>
             <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
                 <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                    <img className="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company"/>
-                    <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account</h2>
+                    <img className="mx-auto h-6 w-auto" src="https://assets-global.website-files.com/6304f37ec5e0ff0b9beb4b6b/6304f3f51e1ba82f2a01eee6_Asset%201.svg" alt="code challenge"/>
+                    <h2 className="mt-5 text-center text-sm font-bold leading-9 tracking-tight text-gray-400">Code challenge: Rick & Morty API</h2>
                 </div>
 
                 <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
@@ -50,13 +49,12 @@ const LoginTemplate = () => {
                     <div>
                         <label for="email" className="block text-sm font-medium leading-6 text-gray-900">Email address</label>
                         <div className="mt-2">
-                            {errors.email && <span className="small">{errors.email}</span>}
+                            {errors.email && <span className="text-[#E10000] text-sm">{errors.email}</span>}
                             <input 
                                 id="Email" 
                                 name="Email" 
                                 type="Email"
                                 ref={inputEmail}
-                                required 
                                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                             />
                         </div>
@@ -65,18 +63,14 @@ const LoginTemplate = () => {
                     <div>
                         <div className="flex items-center justify-between">
                         <label for="password" className="block text-sm font-medium leading-6 text-gray-900">Password</label>
-                        <div className="text-sm">
-                            <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
-                        </div>
                         </div>
                         <div className="mt-2">
-                            {errors.email && <span className="small">{errors.email}</span>}
+                            {errors.email && <span className="text-[#E10000] text-sm">{errors.email}</span>}
                             <input 
                                 id="Password" 
                                 name="Password" 
                                 type="password"
                                 ref={inputPassword}
-                                required 
                                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                             />
                         </div>
@@ -86,11 +80,6 @@ const LoginTemplate = () => {
                         <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
                     </div>
                     </form>
-
-                    <p className="mt-10 text-center text-sm text-gray-500">
-                    Not a member?
-                    <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Start a 14 day free trial</a>
-                    </p>
                 </div>
             </div>        
 		</>
